2c2caf0a3134d4dbac7a283bf187a0064feba321
Prakkmak <Prakkmak@gmail.com>
2019-03-29 22:03:52 +0100

    0.0

---
+++ Lambda.Tests/Entity/CharacterTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AltV.Net.Elements.Entities;
+using Lambda.Entity;
+using Xunit;
+
+namespace Lambda.Tests.Entity
+{
+    public class CharacterTests
+    {
+        [Fact]
+        public void GetCharacter_ShouldWork()
+        {
+            Character character = new Character("Player0", 0);
+            Character character2 = new Character("Player1", 1);
+            Character.Characters.Add(character);
+            Character.Characters.Add(character2);
+            Character actual = Character.GetCharacter(0);
+            Assert.NotNull(actual);
+        }
+        [Fact]
+        public void GetCharacter_ShouldFail()
+        {
+            Character character = new Character("Player0", 0);
+            Character character2 = new Character("Player1", 1);
+            Character.Characters.Add(character);
+            Character.Characters.Add(character2);
+            Character actual = Character.GetCharacter(100);
+            Assert.Null(actual);
+        }
+        [Fact]
+        public void GetCharacters_ShouldWork()
+        {
+            Character character = new Character("Player0", 0);
+            Character character2 = new Character("Player1", 1);
+            Character.Characters.Add(character);
+            Character.Characters.Add(character2);
+            Character[] actual = Character.GetCharacters("Pla");
+            Assert.True(actual.Length == 2);
+        }
+        [Fact]
+        public void GetCharacters_ShouldFail()
+        {
+            Character character = new Character("Player0", 0);
+            Character character2 = new Character("Player1", 1);
+            Character.Characters.Add(character);
+            Character.Characters.Add(character2);
+            Character[] actual = Character.GetCharacters("NotGoodName");
+            Assert.True(actual.Length == 0);
+        }
+    }
+}
---
+++ Lambda.Tests/Utils/ChatTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lambda.Administration;
+using Lambda.Commands;
+using Lambda.Utils;
+using Xunit;
+
+namespace Lambda.Tests.Utils
+{
+    public class ChatTests
+    {
+        [Fact]
+        public void InvokeCmd_ShouldWork()
+        {
+            Command cmd = new Command("test", (
+                (account1, parameters) => new CmdReturn("test_return", CmdReturn.CmdReturnType.SUCCESS)),
+                Command.CommandType.TEST, new[] { "arg1" });
+            Command.Commands.Add(cmd);
+            Account account = new Account();
+            string msg = "/test arg1";
+            CmdReturn actual = Chat.InvokeCmd(account, msg);
+            Assert.Equal(CmdReturn.CmdReturnType.SUCCESS, actual.Type);
+        }
+    }
+}
